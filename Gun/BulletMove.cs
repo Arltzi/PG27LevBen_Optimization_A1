2a515e0f8a1ac0b0c1436ae10dcfd45e577f55ba
Lev Zitron <pg27lev@vfs.com>
2025-05-21 17:43:31 -0700

    init commit

---
+++ Gun/BulletMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void Update()
+    {
+        this.transform.Translate(0, 0, 0.5f);
+    }
+}
