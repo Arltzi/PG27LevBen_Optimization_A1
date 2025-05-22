3c5de837a6467ef91c66000a4eda4b041666e363
Lev Zitron <pg27lev@vfs.com>
2025-05-21 18:47:52 -0700

    start turretplacer

---
+++ Scripts/TurretPlacer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TurretPlacer : MonoBehaviour
+{
+    public float turretGenRadius = 100.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
